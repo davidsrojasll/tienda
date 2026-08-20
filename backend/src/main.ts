9d2257d5fbbef4467830a6d43c9b1052b3bc4e95
David Rojas <davidsrojasll@gmail.com>
2026-08-20 09:44:25 -0500

    feat: connect frontend to backend with CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3001', // solo tu frontend local
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
